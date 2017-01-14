a1fcafd737c27ec13704b78154a8c30bfaff53d6
Jonathan Beard <jonathan.c.beard@gmail.com>
2017-01-14 09:09:34 -0600

    fixed input sanitization problem, added timing, and fixed a few bugs...need to clean up code later..

--- math.cpp
+++ math.cpp
@@ -28,6 +28,7 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <cstdio>
 #include <cstdint>
 #include <iomanip>
 #include <array>
@@ -44,11 +45,54 @@
 #include <unistd.h>
 #include <iomanip>
 #include <thread>
+#include <limits>
+#include <float.h>
+
+/** constants **/
+const static  std::string BLUE          = "\033[1;36m";
+const static  std::string NORMAL        = "\033[0m";
+const static  std::string RED           = "\033[1;31m";
+const static  std::string LIGHT_GREY    = "\033[0;37m";
+const static  std::string PURPLE        = "\033[1;35m";
 
 /** unfortunate, but sig handler so...**/
-static std::ofstream userlog;
-static double fraction_correct = 0.0;
+static std::ofstream    userlog;
+
+struct STAT{
+    double           fraction         = 0.0;
+    double           min_time         = DBL_MAX;
+    double           max_time         = -DBL_MAX;
+    double           avg_time         = 0.0;
+    std::uint32_t    count            = 0;
+    double           tot_time         = 0.0;
+    static std::uint32_t total;
+} correct, incorrect;
+
+std::uint32_t STAT::total = 0;
+
 decltype( std::chrono::system_clock::now() ) start;
+
+static void updateStats( STAT &st, const double curr_dur ) noexcept
+{
+    st.count++;
+    st.tot_time += curr_dur;
+    if( st.min_time > curr_dur ){ st.min_time = curr_dur; }
+    if( st.max_time < curr_dur ){ st.max_time = curr_dur; }
+    st.avg_time = st.tot_time / st.count;
+    STAT::total++;
+    st.fraction =(double) st.count / (double) STAT::total;
+    return;
+}
+
+static void printStats( const std::string &&type, const STAT &st, std::ostream &stream )
+{
+    stream << PURPLE << type << " stats:" << NORMAL << "\n";
+    stream << "perecentage: " <<  std::setprecision( 4 ) << (st.fraction) << "\n";
+    stream << "min time: " << std::setprecision( 2 ) << st.min_time << "\n";
+    stream << "avg time: " << std::setprecision( 2 ) << st.avg_time << "\n";
+    stream << "max time: " << std::setprecision( 2 ) << st.max_time << "\n\n";
+}
+
 
 static
 void sig_handler( int sig )
@@ -59,11 +103,35 @@ void sig_handler( int sig )
       userlog.close();
    }
    std::cout << "\n\n";
-   std::cout << "Your % correct is: " << std::setprecision( 5 ) << 
-      fraction_correct << std::endl;
+   printStats( "correct", correct, std::cout );
+   printStats( "incorrect", incorrect, std::cout );
    //just in case we interrupted the say cmd
    unlink( "/tmp/speach" );
    exit( EXIT_SUCCESS );
+}
+
+
+char* getInput( char *buffer, const std::size_t length, std::size_t &ret )
+{
+    bool invalid = false;
+    std::size_t curr_pos = 0;
+    while( curr_pos < length )
+    {
+        buffer[ curr_pos ] = getc( stdin );
+        if( buffer[ curr_pos ] == '\n' )
+        {
+            ret = curr_pos;
+            break;
+        }
+        else if( ( buffer[ curr_pos ] <  0x30 || buffer[ curr_pos ] > 0x39 ) && buffer[ curr_pos ] != 0x2d )
+        {
+            /** invalid input **/
+            invalid = true;
+        }
+        curr_pos++;
+    }
+    if( invalid ){ return( nullptr ); }
+    return( buffer );
 }
 
 class problem
@@ -103,11 +171,30 @@ protected:
                   " " + std::to_string( b ) );
    }
 
+   static 
+   std::string
+   fix_up_neg( const std::string &str )
+   {
+        std::stringstream ss;
+        for( auto i( 0 ); i < str.length(); i++ )
+        {
+            if( str[ i ] == '-' && i == 0 )
+            {
+                ss << "negative ";
+            }
+            else
+            {
+                ss << str[ i ];
+            }
+        }
+        return( ss.str() );
+   }
+
    static
    void do_speech( const std::string str )
    {
    	std::ofstream ofs( "/tmp/speach" );
-   	ofs << str;
+   	ofs << fix_up_neg( str );
    	ofs.close();
    	system( "say -f /tmp/speach" );
    	unlink( "/tmp/speach" );
@@ -125,28 +212,49 @@ protected:
    template < typename T, 
               typename std::enable_if< 
                std::is_fundamental< T >::value >::type* = nullptr >
-   bool check_ans( const T &ans, 
-                   const T &key ) 
+   bool check_ans( const std::string &prob,
+                   const T &ans, 
+                   const T &key,
+                   double  &time,
+                   std::ostream &logstream ) 
    {
       auto end = std::chrono::system_clock::now();
       std::chrono::duration< double > diff = end-start;
+      time = diff.count();
       if( ans  == key )
       {
         std::stringstream ss;
-        ss <<  ans  <<  " is correct in " << 
-            std::setprecision(2) << diff.count() << 
-                " seconds , GOOD JOB!!" << std::endl;	
+        ss << BLUE <<  ans  <<  " is correct " << NORMAL << "in " << 
+            std::setprecision(2) << time << 
+                " seconds, " << PURPLE << "GOOD JOB!!" << NORMAL << std::endl;	
 
         output_stream << ss.str() << "\n";
-      	do_speech( ss.str() );
+        
+        std::stringstream speach_ss;
+        speach_ss <<  ans  <<  " is correct "  << "in " << 
+            std::setprecision( 2 ) << time << 
+                " seconds, " << "GOOD JOB!!" << std::endl;	
+      	updateStats( correct, time);
+        logstream << BLUE << "correct,  " << prob << ", " << ans << ", " << time << std::endl;
+
+        do_speech( speach_ss.str() );
         return( true );
       }
+
       std::stringstream ss;
-      ss << ans <<  " is incorrect in " << 
-        std::setprecision( 2 ) << diff.count() << 
-            " seconds, lets maybe try another" << std::endl;
+      ss << RED << ans <<  " is incorrect " << NORMAL << "in " << 
+        std::setprecision( 2 ) << time << 
+            " seconds, lets try another" << std::endl;
       output_stream << ss.str() << "\n";
-      do_speech( ss.str() );
+      
+      std::stringstream speach_ss;
+      speach_ss << ans <<  " is incorrect " << "in " << 
+        std::setprecision( 2 ) << time << 
+            " seconds, lets maybe try another" << std::endl;
+      updateStats( incorrect, time );
+      
+      logstream << BLUE << "incorrect,  " << prob << ", " << ans << ", " << time << std::endl;
+      do_speech( speach_ss.str() );
       return( false );
    }
 
@@ -210,46 +318,25 @@ public:
          num_a = num_gen( gen );
 	     num_b = num_gen( gen );
       }
-      count++;
       
-      auto function([&]( const std::int64_t numA, const std::int64_t numB ) -> void
+      auto function([&p]( const std::int64_t numA, const std::int64_t numB ) -> void
       {
-        if( p->run( numA, numB ) )
-        {
-           correct++;
-        }
+         p->run( numA, numB );
         return;
       } );
 
       start = std::chrono::system_clock::now();
       std::thread th( function, num_a, num_b );
-      //FIXME, come back here
+      
       th.join();
    }
 
-   std::uint32_t getCount()
-   {
-      return( count );
-   }
 
-   double getPercentageCorrect()
-   {
-      if( count == 0 )
-      {
-         return( 0.0 );
-      }
-      else
-      {
-         return( ( (double)correct / (double)count ) * 100.00 );
-      }
-   }
 private:
    std::vector< problem* > probs;
    std::default_random_engine gen;
    const std::int64_t min;
    const std::int64_t max;
-   std::uint32_t      correct = 0;
-   std::uint32_t      count   = 0;
 };
 
 class add : public problem
@@ -265,24 +352,19 @@ public:
       ask_prob( prob );
       do_speech( what + prob + " equal" );
       std::int64_t ans( 0 );
-      std::cin >> ans;
-      if( check_ans( ans, key ) )
+      char buffer[ 128 ];
+      std::memset( buffer, '\0', 128 );
+      std::size_t ret_count( 0 );
+      char *text_ans( nullptr );
+      while( ret_count == 0 )
       {
-         
-         if( logstream.good() )
-         {
-            logstream << "correct,   " << prob << ", " << ans << std::endl;
-         }
-         return( true );
+         text_ans = getInput( buffer, 128, ret_count );
+         if( text_ans == nullptr && ret_count != 0 ){ std::cout << "invalid input detected, please only numbers\n"; ret_count = 0;}
+         /** else spin for answer **/
       }
-      else
-      {
-         if( logstream.good() )
-         {
-            logstream << "incorrect, " << prob << ", " << ans << std::endl;
-         }
-         return( false );
-      }
+      ans = strtoll( text_ans, nullptr, 10);
+      double time( 0.0 );
+      return( check_ans( prob, ans, key, time, logstream ) );
    }
 };
 
@@ -315,24 +397,20 @@ public:
       const auto key( a - b );
       ask_prob( prob );
       do_speech( what + prob_spoken + " equal" );
+      double time( 0.0 );
       std::int64_t ans( 0 );
-      std::cin >> ans;
-      if( check_ans( ans, key ) )
+      char buffer[ 128 ];
+      std::memset( buffer, '\0', 128 );
+      std::size_t ret_count( 0 );
+      char *text_ans( nullptr );
+      while( ret_count == 0 )
       {
-         if( logstream.good() )
-         {
-            logstream << "correct,   " << prob << ", " << ans << std::endl;
-         }
-         return( true );
+         text_ans = getInput( buffer, 128, ret_count );
+         if( text_ans == nullptr && ret_count != 0 ){ std::cout << "invalid input detected, please only numbers\n"; ret_count = 0;}
+         /** else spin for answer **/
       }
-      else
-      {
-         if( logstream.good() )
-         {
-            logstream << "incorrect, " << prob << ", " << ans << std::endl;
-         }
-         return( false );
-      }
+      ans = strtoll( text_ans, nullptr, 10);
+      return( check_ans( prob, ans, key, time, logstream ) );
    }
 private:
    const bool noneg;
@@ -345,6 +423,11 @@ main( int argc, char **argv )
 {
    
    if( signal( SIGINT, sig_handler ) == SIG_ERR )
+   {
+      perror( "Failed to set signal handler, exiting!" );
+      exit( EXIT_FAILURE );
+   }
+   if( signal( SIGQUIT, sig_handler ) == SIG_ERR )
    {
       perror( "Failed to set signal handler, exiting!" );
       exit( EXIT_FAILURE );
@@ -426,15 +509,15 @@ main( int argc, char **argv )
    }
 
 	
-   while( blackboard.getCount() < num_problems  || fraction_correct < frac_to_exit )
+   while( (correct.count + incorrect.count) < num_problems  || 
+           correct.fraction < frac_to_exit )
    {
       blackboard();
-	  fraction_correct = blackboard.getPercentageCorrect();
    }
 
    if( userlog.is_open() )
    {
       userlog.close();
    }
-	return( EXIT_SUCCESS );
+   return( EXIT_SUCCESS );
 }
